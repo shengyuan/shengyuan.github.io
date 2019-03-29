b37a03459a8abcd56a7bc10de68d1a57a9676d48
sheng <karokya@gmail.com>
2019-03-29 15:15:02 +0800

    test

--- webduino-module-imageml/blockly/javascript.js
+++ webduino-module-imageml/blockly/javascript.js
@@ -1,4 +1,5 @@
 Blockly.JavaScript['imageml2_classifier'] = function (block) {
+  console.log(1);
   var text_cameraurl = block.getFieldValue('cameraURL');
   var text_camSource = block.getFieldValue('camSource');
   var text_modelname = block.getFieldValue('modelName');
@@ -7,6 +8,7 @@ Blockly.JavaScript['imageml2_classifier'] = function (block) {
 };
 
 Blockly.JavaScript['imageml2_callback'] = function (block) {
+  console.log(2);
   var idx = block.getFieldValue('idx');
   var variable_name = Blockly.JavaScript.variableDB_.getName(block.getFieldValue('name'), Blockly.Variables.NAME_TYPE);
   var statements_name = Blockly.JavaScript.statementToCode(block, 'name');
@@ -17,12 +19,14 @@ Blockly.JavaScript['imageml2_callback'] = function (block) {
 };
 
 Blockly.JavaScript['imageml2_levelVal'] = function (block) {
+  console.log(3);
   var variable_name = Blockly.JavaScript.variableDB_.getName(block.getFieldValue('name'), Blockly.Variables.NAME_TYPE);
   var code = variable_name + '.getClass()';
   return [code, Blockly.JavaScript.ORDER_NONE];
 };
 
 Blockly.JavaScript['imageml2_confidenceVal'] = function (block) {
+  console.log(4);
   var variable_name = Blockly.JavaScript.variableDB_.getName(block.getFieldValue('name'), Blockly.Variables.NAME_TYPE);
   var code = variable_name + '.getConfidence()';
   return [code, Blockly.JavaScript.ORDER_NONE];
